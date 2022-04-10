7f0518c5e96a2e1ce0c69bef440e2091841bb24a
SajadRahimi1 <sajadrahimi0101@gmail.com>
2022-04-10 13:52:35 +0430

    created some files that need it

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { PlaceModule } from './place/place.module';
+import { CommentModule } from './comment/comment.module';
+import { TourModule } from './tour/tour.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, PlaceModule, CommentModule, TourModule],
   controllers: [AppController],
   providers: [AppService],
 })
